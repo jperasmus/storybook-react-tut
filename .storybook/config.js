e21184ac500cb5f737f0d9cd016427e88efe912d
JP Erasmus <jperasmus11@gmail.com>
2019-09-10 20:59:24 +0200

    task: add storybook chromatic plugin

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,6 @@
 import { configure } from '@storybook/react';
-import requireContext from 'require-context.macro'
+import requireContext from 'require-context.macro';
+import 'storybook-chromatic';
 import '../src/index.css';
 
 const req = requireContext('../src/components', true, /\.stories\.js$/);
